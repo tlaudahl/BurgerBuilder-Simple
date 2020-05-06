9e9886cdd5482ff978a5312997a1830d6f238011
tlaudahl <travis.laudahl@gmail.com>
2020-05-06 12:50:00 -0500

    Added a custom button component for the Modal to continue with the order or cancel the order. Currently continuing with the order only causes an alert to pop up saying You Continue Will code functionallity later. Cancelling an order just closes the modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Button from "../../UI/Button/Button";
+
 export default function OrderSummary(props) {
   const ingredientSummary = Object.keys(props.ingredients).map((key) => (
     <li key={key}>
@@ -9,10 +11,16 @@ export default function OrderSummary(props) {
   ));
   return (
     <>
-      <h3>Your Order</h3>
+      <h3>Your Order Total: ${props.totalPrice.toFixed(2)}</h3>
       <p>A delicious burger with the following ingredients: </p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button clicked={props.purchaseCancelled} btnType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked={props.purchaseContinue} btnType="Success">
+        CONTINUE
+      </Button>
     </>
   );
 }
