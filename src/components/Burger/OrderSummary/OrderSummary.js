be5833a9ae67a25f98f33299a87f80f9e78943e0
tlaudahl <travis.laudahl@gmail.com>
2020-05-11 12:44:10 -0500

    Added final touches from the Section 8 Module

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Button from "../../UI/Button/Button";
 
-export default function OrderSummary(props) {
-  const ingredientSummary = Object.keys(props.ingredients).map((key) => (
-    <li key={key}>
-      <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
-      {props.ingredients[key]}
-    </li>
-  ));
-  return (
-    <>
-      <h3>Your Order Total: ${props.totalPrice.toFixed(2)}</h3>
-      <p>A delicious burger with the following ingredients: </p>
-      <ul>{ingredientSummary}</ul>
-      <p>Continue to Checkout?</p>
-      <Button clicked={props.purchaseCancelled} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={props.purchaseContinue} btnType="Success">
-        CONTINUE
-      </Button>
-    </>
-  );
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((key) => (
+      <li key={key}>
+        <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
+        {this.props.ingredients[key]}
+      </li>
+    ));
+    return (
+      <>
+        <h3>Your Order Total: ${this.props.totalPrice.toFixed(2)}</h3>
+        <p>A delicious burger with the following ingredients: </p>
+        <ul>{ingredientSummary}</ul>
+        <p>Continue to Checkout?</p>
+        <Button clicked={this.props.purchaseCancelled} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={this.props.purchaseContinue} btnType="Success">
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
 }
+
+export default OrderSummary;
