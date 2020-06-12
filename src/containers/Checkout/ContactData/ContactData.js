b2a6298844b46b7e7c57d52232ffc40682b52119
Travis Laudahl <travis.laudahl@gmail.com>
2020-06-11 19:22:18 -0500

    Added an input component to use with contactdata form, also styled the input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 import styles from './ContactData.module.css'
 
@@ -46,10 +47,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={styles.Input} type='text' name='name' placeholder='Your Name' />
-                <input className={styles.Input} type='text' name='email' placeholder='Your Email' />
-                <input className={styles.Input} type='text' name='street' placeholder='Street' />
-                <input className={styles.Input} type='text' name='postal' placeholder='Postal Code' />
+                <Input inputtype='input' type='text' name='name' placeholder='Your Name' />
+                <Input inputtype='input' type='text' name='email' placeholder='Your Email' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='text' name='postal' placeholder='Postal Code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
